4c70462404e355e748199bf9a8be9338e899fa81
Phuc Bui <phuc_bui7718@yahoo.com>
2016-03-02 20:13:08 -0500

    Created the whole Restful API.

--- app/src/main/java/repositories/DriverRepository.java
+++ app/src/main/java/repositories/DriverRepository.java
@@ -1,22 +1,12 @@
-//package repositories;
-//
-//import android.widget.Switch;
-//
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//import java.util.List;
-//
-//import entities.Driver;
-//
-//
-///**
-// * Created by Phuc on 2/23/2016.
-// */
-//
-//public interface DriverRepository  extends MongoRepository <Driver, String> {
-//
-//
-//    public Driver findByFirstName (String firstName);
-//    public List<Driver>  findByLastName (String lastName);
-//
-//}
+package repositories;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import entities.Driver;
+
+public interface DriverRepository extends MongoRepository<Driver, String>{
+
+    Driver findByDriverNum(String driverNum);
+
+}
