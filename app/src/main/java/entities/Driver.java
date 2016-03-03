6ccffbefb4316d9e5dee5d35f6c09d3af80b68c0
Phuc Bui <phuc_bui7718@yahoo.com>
2016-03-02 19:28:53 -0500

    Created a Google App Engine backend and finalized Spring Security stuff.

--- app/src/main/java/entities/Driver.java
+++ app/src/main/java/entities/Driver.java
@@ -5,6 +5,22 @@ package entities;
  */
 public class Driver {
 
+    private String drivernum;
+    private String password;
 
+    public String getDrivernum() {
+        return drivernum;
+    }
 
+    public void setDrivernum(String drivernum) {
+        this.drivernum = drivernum;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
