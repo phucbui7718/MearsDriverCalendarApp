a17093b184b6a372b10743214d28ea616cd7865f
Will <willthomas1979@gmail.com>
2016-03-07 22:32:19 -0500

    Updated with URI

--- app/src/main/java/repositories/MongoDBConnection.java
+++ app/src/main/java/repositories/MongoDBConnection.java
@@ -1,13 +1,16 @@
 package repositories;
 
 import com.mongodb.DB;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
 
 
 
 
 public class MongoDBConnection {
 
-        MongoClient mongoClient = new MongoClient("mongodb://admin:admin@ds019038.mlab.com:19038/mearsdriverdb");
+//    MongoClient mongoClient = new MongoClient("mongodb://admin:admin@ds019038.mlab.com:19038/mearsdriverdb");
 
+    String textUri = "mongodb://admin:admin@ds019038.mlab.com:19038/mearsdriverdb";
+    MongoClientURI uri = new MongoClientURI(textUri);
+    MongoClient m = new MongoClient(uri);
 }
